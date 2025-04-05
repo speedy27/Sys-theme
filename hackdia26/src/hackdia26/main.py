f407808bdb90dd0a9bb8929c20d0399fd9a0cf97
speedy27 <98963154+speedy27@users.noreply.github.com>
2025-04-05 14:01:30 +0200

    test1

--- hackdia26/src/hackdia26/main.py
+++ hackdia26/src/hackdia26/main.py
@@ -9,7 +9,7 @@ from hackdia26.crew import Hackdia26
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
 # This main file is intended to be a way for you to run your
-# crew locally, so refrain from adding unnecessary logic into this file.
+# crew l ocally, so refrain from adding unnecessary logic into this file.
 # Replace with inputs you want to test with, it will automatically
 # interpolate any tasks and agents information
 
